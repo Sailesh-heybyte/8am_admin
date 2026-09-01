b7479c8f325554440087b31e913b5f6559452f33
Krishna3968 <131745597+Krishna3968@users.noreply.github.com>
2026-09-01 16:56:26 +0530

    feat: add users & roles screen

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Schools from "./screens/adminLogin/Schools/index.jsx";
 import SchoolGroups from "./screens/adminLogin/SchoolGroups/index.jsx";
 import Buses from "./screens/adminLogin/Buses/index.jsx";
 import Drivers from "./screens/adminLogin/Drivers/index.jsx";
+import UsersRoles from "./screens/adminLogin/UsersRoles/index.jsx";
 
 const menuItems = [
   {
@@ -78,6 +79,7 @@ function App() {
       "school-groups": <SchoolGroups />,
       buses: <Buses />,
       drivers: <Drivers />,
+      "users-roles": <UsersRoles />,
     };
     return pages[activePage] || <Dashboard />;
   };
